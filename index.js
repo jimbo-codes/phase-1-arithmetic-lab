f48672b9a9ad5d061930493a09e00e881c6e5041
James Burns <jrburns96@aol.com>
2021-11-01 13:41:22 -0400

    Math

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+const num1=2;
+const num2=31;
+const num3=6;
+const num4=22;
+let multiply = num1*num2;
+let random = Math.ceil((Math.random()*10));
+let mod = num4%num3;
+let max = Math.max(num1,num3,20);
